d144ead1d601758c60d67ee3805e9294fa56b760
mokautsar30 <mohammadkautsar30@gmail.com>
2024-06-07 14:37:03 +0700

    feat: user-setting-layout

--- client/dashboard/src/components/topSection/Activity.jsx
+++ client/dashboard/src/components/topSection/Activity.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Activity = () => {
   return (
-    <div>Activity</div>
-  )
-}
+    <div className="py-2 px-4 min-w-full bg-white rounded-lg shadow-lg overflow-hidden">
+      Activity
+      <div>test</div>
+      <div>test</div>
+      <div>test</div>
+      <div>test</div>
+      <div>test</div>
+      <div>test</div>
+      <div>test</div>
+    </div>
+  );
+};
 
-export default Activity
+export default Activity;
--- client/dashboard/src/pages/home/Homepage.jsx
+++ client/dashboard/src/pages/home/Homepage.jsx
@@ -8,10 +8,10 @@ const Homepage = () => {
       <Navbar />
       <div className="flex flex-col bg-slate-100">
         <h1 className=" text-3xl text-black font-bold px-4 py-4 font-nunito">HH Timesheet</h1>
-        <div className=" px-6">
+      </div>
+        <div className=" py-2 px-4 min-w-full">
           <ColorTabs />
         </div>
-      </div>
     </>
   );
 };
